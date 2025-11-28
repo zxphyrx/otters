0b1b06fd1cb31191bba523fe8d19173e95e16fcb
Zeph Requilme <zephrequilme40@gmail.com>
2025-11-28 23:02:22 +0800

    Setup svelte and started on the hero

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,4 @@
+<div class="section">
+    <h1>Sea Otters</h1>
+    <p>The sea otter (Enhydra Lutris) is a marine mammal native to the coasts of the northern and eastern Northern Pacific Ocean.</p>
+</div>
